8e88a04733b06a0a36901a764e2c8e9206b4c20e
Uchenna Obicheta <uobicheta@hotmail.com>
2023-04-14 18:27:33 -0400

    fixed conflict errors in webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -24,8 +25,8 @@ module.exports = () => {
     }),
     new MiniCssExtractPlugin(),
     new InjectManifest({
-      swSrc: './src/sw.js',
-      swDest: 'service-worker.js',
+      swSrc: './src-sw.js',
+      swDest: 'sw.js',
     }),
 
     new GenerateSW(),
