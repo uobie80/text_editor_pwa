a59151cca44a4ea0ed3cce8ae07631ed5530aefd
Uchenna Obicheta <uobicheta@hotmail.com>
2023-04-14 15:04:29 -0400

    Defined rules in webpack.config.js.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,9 +31,25 @@ module.exports = () => {
     ],
 
     module: {
-      rules: [
-        
-      ],
-    },
-  };
+      rules: [ {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+ };
 };
